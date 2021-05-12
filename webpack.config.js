eea5797a2710a4d47954006c155bc2a43027ecb6
fetch-ai <fetch@slowfis.tv>
2021-05-11 23:38:05 -0700

    removed desktop and ingame files

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    background: './windows/background/background.ts',
-    desktop: './windows/desktop/desktop.ts',
-    in_game: './windows/in_game/in_game.ts',
+    background: './src/windows/background/background.ts',
   },
   devtool: 'inline-source-map',
   module: {
@@ -14,29 +12,33 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   resolve: {
     extensions: ['.ts'],
   },
   output: {
-    path: `${__dirname}/dist`,
+    path: `${__dirname}/src/dist`,
     filename: '[name]/[name].js',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './windows/background/background.html',
-      filename: `${__dirname}/dist/background/background.html`,
+      template: './src/windows/background/background.html',
+      filename: `${__dirname}/src/dist/background/background.html`,
       chunks: ['background'],
     }),
     new HtmlWebpackPlugin({
-      template: './windows/desktop/desktop.html',
-      filename: `${__dirname}/dist/desktop/desktop.html`,
+      template: './src/windows/desktop/desktop.html',
+      filename: `${__dirname}/src/dist/desktop/desktop.html`,
       chunks: ['desktop'],
     }),
     new HtmlWebpackPlugin({
-      template: './windows/in_game/in_game.html',
-      filename: `${__dirname}/dist/in_game/in_game.html`,
+      template: './src/windows/in_game/in_game.html',
+      filename: `${__dirname}/src/dist/in_game/in_game.html`,
       chunks: ['in_game'],
     }),
   ],
